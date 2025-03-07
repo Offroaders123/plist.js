2b847ee5289148cab693165ce69fed6c15e80fd8
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-03-06 17:00:51 -0800

    Typings
    Started working on these last week, committing them now.
    
    Sunday, March 2, 2025 at 12:52 AM
    
    The `types.d.ts` file is from `@types/plist`, I am using it as a reference for how the internal code should be typed.

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 /**
+ * @typedef {string | number | boolean | Date | Buffer | PlistObject | PlistArray} PlistValue
+*/
+
+/**
+ * @typedef {{ readonly [x: string]: PlistValue; }} PlistObject
+*/
+
+/**
+ * @typedef {ReadonlyArray<PlistValue>} PlistArray
+*/
+
+/**
  * Parser functions.
  */
 
 var parserFunctions = require('./lib/parse');
-Object.keys(parserFunctions).forEach(function (k) { exports[k] = parserFunctions[k]; });
+// Object.keys(parserFunctions).forEach(function (k) { exports[k] = parserFunctions[k]; });
+
+exports.parse = parserFunctions.parse;
 
 /**
  * Builder functions.
  */
 
 var builderFunctions = require('./lib/build');
-Object.keys(builderFunctions).forEach(function (k) { exports[k] = builderFunctions[k]; });
+// Object.keys(builderFunctions).forEach(function (k) { exports[k] = builderFunctions[k]; });
+
+exports.build = builderFunctions.build;
